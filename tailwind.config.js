9a48fa6eb4bdb104569ace4ed67b9a35df3cea60
Hrsh Gupta <gharsh11032000@gmail.com>
2022-08-14 19:40:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#074450",
+        secondaryColor: "#0b7285",
+        tertiaryColor: "#E2DCC8",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
